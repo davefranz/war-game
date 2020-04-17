95ff75a428cdf3606665064e4a619bc4eae0bb04
Dave Franz <davidlawrencefranz@gmail.com>
2020-04-17 11:19:42 -0700

    cleaned up rules for images

--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,7 @@ module.exports = {
     rules: [
       { test: /\.(js|jsx)$/, use: 'babel-loader' },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.(png|jpe?g|gif)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-        },
-      ],
-      },
+      { test: /\.(png|jpe?g|gif)$/i, use: [{ loader: 'file-loader', },], },
     ]
   },
   mode: 'development',
