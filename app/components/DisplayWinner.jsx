08cb3544b0256b22bd95e7dd9556447eef5af969
Benjamin Verble <BenjaminVerble@users.noreply.github.com>
2020-05-02 22:16:19 -0700

    add some feedback

--- app/components/DisplayWinner.jsx
+++ app/components/DisplayWinner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// this is a little small to be its own component
 const DisplayWinner = ({ winner }) => {
   if (!winner) return <p id="winner">Click Battle to Begin</p>;
   else if (winner.length > 1) return <p id="winner">{winner}!</p>;
--- app/containers/GameBoard.jsx
+++ app/containers/GameBoard.jsx
@@ -39,23 +39,48 @@ class GameBoard extends Component {
   createGame() {
     const deck = createDeck();
     const shuffled = shuffleDeck(deck);
+    // in this case, I wouldn't pass this.state.numOfPlayers
+    // to dealDeck as a parameter since your dealDeck
+    // method has access to that. it will be cleaner that way.
+    // You want to parameterize things moreso when you want 
+    // to extract that function or method from this class
     this.dealDeck(shuffled, this.state.numOfPlayers)
   }
 
+  // name your variables and parameters as specifically as you can
+  // array is too vague
   dealDeck(array, numOfPlayers) {
     const deck1 = [];
     const deck2 = [];
     const deck3 = [];
-    const deck4 = [];
+    const deck4 = []; 
+    /*
+      suggestion part one
+      const decks = {};
+    */
     let i = 1;
     while (array.length > 0) {
       const card = array.pop();
+      /*
+        suggestion part two
+        const currentDeck = decks[`deck{i}`]
+        if (currentDeck === undefined) {
+          currentDeck = [card];
+        } else {
+          currentDeck.push(card);
+        }
+      */
+      // This is a little too "brute force"
+      // See suggestions part 1 and two above
       if (i === 1) deck1.push(card);
       else if (i === 2) deck2.push(card);
       else if (i === 3) deck3.push(card);
       else if (i === 4) deck4.push(card);
       i += 1;
       if (i > numOfPlayers) i = 1;
+      // instead of the previous two lines,
+      // you could also do
+      // i = i === numOfPlayers ? 1 : i + 1;
       if (array.length === 0) break;
     }
     this.setState(
@@ -66,6 +91,7 @@ class GameBoard extends Component {
   }
 
   // Pops a card from each deck to be played and determines winner
+  // array is to vague of a parameter name
   battle(array) {
     const cards = [];
     array.forEach(deck => {
@@ -86,13 +112,20 @@ class GameBoard extends Component {
   // checks if more than one player has the highest card
   compareWinnerAndDuplicates(array, valueOfDuplicates, currCards) {
     const { decks, cardsToBeWon } = this.state;
+
     const totalWin = [];
 
     if (array[1] > valueOfDuplicates) {
-      currCards.forEach(card => {
+      currCards.forEach(card => {  
+        // here you are mutating state
+        // This will not trigger a re-render in react
+        // since you are not calling setState
+        // this can lead to confusing behavior in your component
+        // copy decks. change the part you want, call setState to update the decks
         if (card) decks[array[0]].unshift(card);
       })
       cardsToBeWon.forEach(card => {
+        // same issue here as the loop above
         if (card) decks[array[0]].unshift(card);
       })
       this.setState({
