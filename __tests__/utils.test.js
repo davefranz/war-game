818ef15ce8e83dc03cf629d0e32d3084a3965f2a
Dave Franz <davidlawrencefranz@gmail.com>
2020-05-07 18:47:51 -0700

    rewrote tests

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,20 +1,55 @@
 import { createDeck, shuffleDeck, findWinner, checkForDuplicate } from '../app/utils';
+import { mockRandom, resetMockRandom } from 'jest-mock-random';
 
-describe('unit tests for utility functions', () => {
 
-  it('createDeck returns an array with length 52', () => {
-    const deck = createDeck();
+describe('createDeck function', () => {
+  const deck = createDeck();
+
+  it('should return an array with a length of 52', () => {
+    expect(Array.isArray(deck)).toBe(true)
     expect(deck.length).toBe(52);
-    expect(deck[0]).toBe(2);
-    expect(deck[51]).toBe(14);
-  });
+  })
 
-  it('shuffleDeck randomizes the order of the input array', () => {
+  it('should create an array where every value 2-14 is repeated 4 times', () => {
+    const cardCount = deck.reduce((acc, curCard) => {
+      acc[curCard] = (acc[curCard] || 0) + 1;
+      return acc;
+    }, {} );
+    const fourOfEveryCard = Object.values(cardCount).every(count => count === 4);
+    expect(fourOfEveryCard).toBe(true);
+  })
+})
+
+// OLD CREATEDECK TESTS
+  // it('createDeck returns an array with length 52', () => {
+  //   const deck = createDeck();
+  //   expect(deck.length).toBe(52);
+  //   expect(deck[0]).toBe(2);
+  //   expect(deck[51]).toBe(14);
+  // });
+
+
+describe('shuffleDeck function', () => {
+  it('should reorder the values in the input array', () => {
+    mockRandom([0.01]);
     const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     const shuffled = shuffleDeck(input);
-    expect(shuffled.length).toBe(10);
-    expect(shuffled[0]).not.toBe(1);
-  });
+    expect(shuffled[8]).toEqual(10);  
+    expect(shuffled[0]).not.toEqual(1);
+    expect(shuffled).toEqual(input);
+    resetMockRandom();
+  })
+})
+
+// OLD TEST FOR SHUFFLEDECK
+// it('shuffleDeck randomizes the order of the input array', () => {
+//   const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//   const shuffled = shuffleDeck(input);
+//   expect(shuffled.length).toBe(10);
+//   expect(shuffled[0]).not.toBe(1);
+// });
+
+describe('unit tests for utility functions', () => {
 
   it('findWinner returns the index of largest number in an array and the value', () => {
     const input = [8, 10, 14, 6];
